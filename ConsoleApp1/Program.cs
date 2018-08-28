81473251a889f051290204cea198b3fe04ad9f11
annamd115 <anna.dolan115@gmail.com>
2018-08-28 17:32:52 -0500

    did it

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number");
+            var input = "1,2,3";
+            var total = 1;
+
+            var noCommas = input.Split(',');
+
+            Console.WriteLine("Do you want to multiply or square these numbers? m/s");
+            var answer = Console.ReadLine();
+
+            if (answer == "m")
+            {
+                foreach (var num in noCommas)
+                {
+                    total *= int.Parse(num);
+                    Console.WriteLine(total);
+                }
+            }
+            else if (answer == "s")
+            {
+                var squareNum = 0;
+                var squareArr = "";
+
+                foreach (var num in noCommas)
+                {
+                    var num2 = int.Parse(num);
+                    squareNum = num2 * num2;
+                    squareArr += squareNum.ToString() + ",";
+                }
+                squareArr = squareArr.TrimEnd(new char[] { ',' });
+                Console.WriteLine(squareArr);
+            }
+            Console.ReadLine();
         }
     }
 }
